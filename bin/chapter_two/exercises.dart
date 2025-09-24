e3aff6400c68ae537f6dbb82ec72a2fbfc291e13
Luka <luka.amiranashvili.2001@gmail.com>
2025-09-24 22:38:04 +0400

    feat: wrote logic for chapter 2

--- bin/chapter_two/exercises.dart
+++ bin/chapter_two/exercises.dart
@@ -1,17 +1,64 @@
+import 'dart:io';
 
 // exercises to help students practice control structures in Dart:
 
+void main() {
+  oddOrEven();
+  oneToTen();
+  between();
+}
+
 // 1. Write a program that takes a number as input and prints whether it is odd or even.
+void oddOrEven() {
+  num a = 5;
+  if (a % 2 == 0) {
+    print('odd');
+  } else {
+    print('even');
+  }
+}
 
 // 2. Write a program that takes a number as input and prints all the numbers from 1 to that number.
 
+void oneToTen() {
+  print('enter a number');
+  String? input = stdin.readLineSync();
+  int n = int.parse(input!);
+
+  for (int i = 1; i <= n; i++) {
+    print(i);
+  }
+}
+
 // 3. Write a program that takes two numbers as input and prints all the numbers between them (inclusive).
+
+void between() {
+  print('enter a number');
+  String? input1 = stdin.readLineSync();
+  int n1 = int.parse(input1!);
+
+  String? input2 = stdin.readLineSync();
+  int n2 = int.parse(input2!);
+
+  for (int i = n1; i <= n2; i++) {
+    print(i);
+  }
+}
 
 // 4. Write a program that takes a number as input and calculates its factorial.
 
 // 5. Write a program that takes a number as input and prints all its divisors.
 
 // 6. Write a program that takes a list of numbers as input and finds the largest and smallest number.
+
+void smallNLarge() {
+  List<int> nums = [1, 2, 3, 4];
+  int smallest = nums.reduce((a, b) => a < b ? a : b);
+  int largest = nums.reduce((a, b) => a > b ? a : b);
+
+  print("smallest $smallest");
+  print("largest $largest");
+}
 
 // 7. Write a program that takes a list of numbers as input and sorts them in ascending order.
 
