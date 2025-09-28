9b3a52bd13d1434d6b504ae89891127a1f8083db
Luka <luka.amiranashvili.2001@gmail.com>
2025-09-28 19:26:33 +0400

    feat: added descriptions for better readablity

--- bin/chapter_five/theory_f.dart
+++ bin/chapter_five/theory_f.dart
@@ -1,32 +1,31 @@
-//I believe at this point if you understood everything we covered this should be understandable except:
-// implements: https://www.educative.io/answers/how-to-use-the-implements-keyword-in-dart
-// and
-// typedefs: https://www.tutorialspoint.com/dart_programming/dart_programming_typedef.htm
-// ignore_for_file: unnecessary_final
-
+// Type alias for JSON objects
 typedef Json = Map<String, dynamic>;
 
+// Abstract class defining a contract for converting objects to JSON
 abstract class JsonConvertable<T> {
-  Json toJson();
+  Json toJson(); // Must be implemented by subclasses
 }
 
+// Abstract class defining a contract for returning child objects
 abstract class Parent {
-  getChildsList();
+  getChildsList(); // Must be implemented by subclasses
 }
 
+// Named objects have a name, can convert to JSON, and may have children
 abstract class Named implements JsonConvertable, Parent {
-  final String name;
+  final String name; // Name of the object
 
   const Named({
     required final this.name,
   });
 }
 
+// Base class for celestial objects like stars, planets, moons, and galaxies
 abstract class CelestialObject implements Named {
   @override
-  final String name;
-  final double mass;
-  final double radius;
+  final String name; // Object's name
+  final double mass; // Mass of the object
+  final double radius; // Radius of the object
 
   const CelestialObject({
     required final this.name,
@@ -35,9 +34,10 @@ abstract class CelestialObject implements Named {
   });
 }
 
+// Represents a galaxy containing star systems
 class Galaxy extends CelestialObject {
-  final String color;
-  final List<StarSystem> starSystems;
+  final String color; // Galaxy color
+  final List<StarSystem> starSystems; // List of star systems
 
   const Galaxy({
     required final String name,
@@ -47,6 +47,7 @@ class Galaxy extends CelestialObject {
     required final this.starSystems,
   }) : super(mass: mass, name: name, radius: radius);
 
+  // Creates a Galaxy object from JSON
   @override
   factory Galaxy.fromJson(Json json) {
     return Galaxy(
@@ -60,6 +61,7 @@ class Galaxy extends CelestialObject {
     );
   }
 
+  // Converts Galaxy object to JSON
   @override
   Json toJson() => {
         'name': name,
@@ -70,15 +72,17 @@ class Galaxy extends CelestialObject {
             starSystems.map((starSystem) => starSystem.toJson()).toList(),
       };
 
+  // Returns the list of child star systems
   @override
   List<StarSystem> getChildsList() => starSystems;
 }
 
+// Represents a star system containing a star and planets
 class StarSystem implements Named {
   @override
-  final String name;
-  final Star star;
-  final List<Planet> planets;
+  final String name; // Name of the system
+  final Star star; // Central star
+  final List<Planet> planets; // List of planets
 
   const StarSystem({
     required final this.name,
@@ -86,14 +90,13 @@ class StarSystem implements Named {
     required final this.planets,
   });
 
+  // Creates a StarSystem from JSON
   factory StarSystem.fromJson(Json json) {
     List<Planet> planets = [];
-
     for (var currentRaw in json['planets']) {
       Planet current = Planet.fromJson(currentRaw);
       planets.add(current);
     }
-
     return StarSystem(
       name: json["name"],
       star: Star.fromJson(json["star"]),
@@ -101,6 +104,7 @@ class StarSystem implements Named {
     );
   }
 
+  // Converts StarSystem to JSON
   @override
   Json toJson() => {
         "name": name,
@@ -108,12 +112,14 @@ class StarSystem implements Named {
         "planets": planets.map((planet) => planet.toJson()).toList(),
       };
 
+  // Returns planets as children
   @override
   List<Planet> getChildsList() => planets;
 }
 
+// Represents a star
 class Star extends CelestialObject {
-  final double luminescence;
+  final double luminescence; // Brightness of the star
 
   const Star({
     required final String name,
@@ -122,6 +128,7 @@ class Star extends CelestialObject {
     required final this.luminescence,
   }) : super(mass: mass, name: name, radius: radius);
 
+  // Creates Star from JSON
   @override
   factory Star.fromJson(Json json) => Star(
         name: json['name'],
@@ -130,6 +137,7 @@ class Star extends CelestialObject {
         luminescence: json['luminescence'],
       );
 
+  // Converts Star to JSON
   @override
   Json toJson() => {
         'name': name,
@@ -138,13 +146,15 @@ class Star extends CelestialObject {
         'luminescence': luminescence,
       };
 
+  // Stars have no child objects
   @override
   getChildsList() => Exception("No Child");
 }
 
+// Represents a planet
 class Planet extends CelestialObject {
-  final bool isPopulated;
-  final List<Moon> moons;
+  final bool isPopulated; // Whether planet has life
+  final List<Moon> moons; // Moons orbiting the planet
 
   const Planet({
     required final String name,
@@ -154,12 +164,12 @@ class Planet extends CelestialObject {
     required final this.moons,
   }) : super(mass: mass, name: name, radius: radius);
 
+  // Creates Planet from JSON
   @override
   factory Planet.fromJson(Json json) {
     List<Moon> moons = [];
-
-    for (var currentRaw in json['moons']) {
-      Moon current = Moon.fromJson(currentRaw);
+    for (var moon in json['moons']) {
+      Moon current = Moon.fromJson(moon);
       moons.add(current);
     }
     return Planet(
@@ -171,6 +181,7 @@ class Planet extends CelestialObject {
     );
   }
 
+  // Converts Planet to JSON
   @override
   Json toJson() => {
         'name': name,
@@ -179,10 +190,13 @@ class Planet extends CelestialObject {
         'isPopulated': isPopulated,
         'moons': moons.map((moon) => moon.toJson()).toList(),
       };
+
+  // Returns moons as children
   @override
   List<Moon> getChildsList() => moons;
 }
 
+// Represents a moon
 class Moon extends CelestialObject {
   const Moon({
     required final String name,
@@ -190,12 +204,14 @@ class Moon extends CelestialObject {
     required final double radius,
   }) : super(mass: mass, name: name, radius: radius);
 
+  // Creates Moon from JSON
   factory Moon.fromJson(Json json) => Moon(
         name: json['name'],
         mass: json['mass'],
         radius: json['radius'],
       );
 
+  // Converts Moon to JSON
   @override
   Json toJson() => {
         'name': name,
@@ -203,6 +219,7 @@ class Moon extends CelestialObject {
         'radius': radius,
       };
 
+  // Moons have no children
   @override
   getChildsList() => Exception("No Child");
 }
